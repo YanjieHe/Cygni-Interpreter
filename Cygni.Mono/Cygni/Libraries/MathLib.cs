33a0938e5235ec2201ea43565512c37cf80a78b4
JoshuaHe <JoshuaHe2015@163.com>
2016-11-10 14:06:08 +0800

    Mono

--- Cygni.Mono/Cygni/Libraries/MathLib.cs
+++ Cygni.Mono/Cygni/Libraries/MathLib.cs
@@ -15,33 +15,33 @@ namespace Cygni.Libraries
 	{
 		public static DynValue sqrt(DynValue[] args)
 		{
-			return DynValue.FromNumber(Math.Sqrt(args[0].AsNumber()));
+			return (Math.Sqrt(args[0].AsNumber()));
 		}
 		public static DynValue abs(DynValue[] args)
 		{
-			return DynValue.FromNumber(Math.Abs(args[0].AsNumber()));
+			return (Math.Abs(args[0].AsNumber()));
 		}
 		public static DynValue log(DynValue[] args)
 		{
 			if (args.Length == 1)
-				return DynValue.FromNumber(Math.Log(args[0].AsNumber()));
+				return (Math.Log(args[0].AsNumber()));
 			if (args.Length == 2)
-				return DynValue.FromNumber(Math.Log(args[0].AsNumber(), args[1].AsNumber()));
+				return (Math.Log(args[0].AsNumber(), args[1].AsNumber()));
 			throw RuntimeException.BadArgsNum ("log", "1 or 2");
 		}
 		public static DynValue max(DynValue[] args)
 		{
 			RuntimeException.FuncArgsCheck (args.Length == 2, "max");
-			return DynValue.FromNumber(Math.Max(args[0].AsNumber(), args[1].AsNumber()));
+			return (Math.Max(args[0].AsNumber(), args[1].AsNumber()));
 		}
 		public static DynValue min(DynValue[] args)
 		{
 			RuntimeException.FuncArgsCheck (args.Length == 2, "min");
-			return DynValue.FromNumber(Math.Min(args[0].AsNumber(), args[1].AsNumber()));
+			return (Math.Min(args[0].AsNumber(), args[1].AsNumber()));
 		}
 		public static DynValue exp(DynValue[] args)
 		{
-			return DynValue.FromNumber(Math.Exp(args[0].AsNumber()));
+			return (Math.Exp(args[0].AsNumber()));
 		}
 		public static DynValue sign(DynValue[] args){
 			return (double)Math.Sign(args[0].AsNumber());
--- Cygni.Mono/Cygni/Libraries/StrLib.cs
+++ Cygni.Mono/Cygni/Libraries/StrLib.cs
@@ -17,14 +17,12 @@ namespace Cygni.Libraries
 		public static DynValue strcat (DynValue[] args)
 		{
 			RuntimeException.FuncArgsCheck (args.Length ==  1, "strcat");
-			//return string.Concat (args.Map (i => i.Value));
 			return string.Concat(args[0].As<DynList>().Select(i=>i.Value));
 		}
 
 		public static DynValue strjoin (DynValue[] args)
 		{
 			RuntimeException.FuncArgsCheck (args.Length == 2, "strjoin");
-			//return string.Join (args [0].AsString (), args.SkipMap (1, i => i.Value));
 			return string.Join (args [0].AsString (), args[1].As<DynList>().Select(i=>i.Value));
 		}
 
@@ -42,9 +40,24 @@ namespace Cygni.Libraries
 		public static DynValue strsplit (DynValue[] args)
 		{
 			RuntimeException.FuncArgsCheck (args.Length > 1, "strsplit");
-			return DynValue.FromList (args [0].AsString ()
-			                         .Split (args.SkipMap (1, i => char.Parse (i.AsString ())))
-			                         .ToDynList (DynValue.FromString));
+			string str = args[0].AsString();
+			string[] result;
+			if (args[1].type == DataType.List){
+				DynList list = args[1].As<DynList>();
+				int n = list.Count;
+				char arr = new char[n];
+				for(int i = 0;i < n;i++)
+					arr[i] = char.Parse(list[i].AsString());
+				result = str.Split(arr);
+			}
+			else {
+				int n = args.Length - 1;
+				char arr = new char[n];
+				for(int i = 0;i < n;i++)
+					arr[i] = char.Parse(args[i+1].AsString());
+				result = str.Split(arr);
+			}
+			return DynValue.FromList(result.ToDynList(DynValue.FromString));
 		}
 
 		public static DynValue strrpl (DynValue[] args)
@@ -99,7 +112,26 @@ namespace Cygni.Libraries
 			else
 				return args [0].AsString ().Trim (args.SkipMap (1, i => char.Parse (i.AsString ())));
 		}
+		
+		public static DynValue TrimStart (DynValue[] args)
+		{
+			RuntimeException.FuncArgsCheck (args.Length >= 1, "string.trimStart");
+			string str = args[0].AsString();
+			if (args.Length == 1)
+				return str.TrimStart();
+			else
+				return str.TrimStart(args.SkipMap(1, i => char.Parse(i.AsString())));
+		}
 
+		public static DynValue TrimEnd (DynValue[] args)
+		{
+			RuntimeException.FuncArgsCheck (args.Length >= 1, "string.trimEnd");
+			string str = args[0].AsString();
+			if (args.Length == 1)
+				return str.TrimEnd();
+			else
+				return str.TrimEnd(args.SkipMap(1, i => char.Parse(i.AsString())));
+		}
 		public static DynValue SubString (DynValue[] args)
 		{
 			RuntimeException.FuncArgsCheck (args.Length == 2 || args.Length == 3, "string.subString");
@@ -108,5 +140,7 @@ namespace Cygni.Libraries
 			else
 				return args [0].AsString ().Substring ((int)args [1].AsNumber (), (int)args [2].AsNumber ());
 		}
+
+		
 	}
 }
